169433871d54c27e49e5ae53ddd66d657ffec4c7
neilwalts <neil.walts@gmail.com>
2017-09-12 16:08:01 +0100

    minor update to see GitHub integration working

--- src/ubs/walterne/berlinClock/BerlinClockFace.java
+++ src/ubs/walterne/berlinClock/BerlinClockFace.java
@@ -1,6 +1,7 @@
 package ubs.walterne.berlinClock;
 
 // Returns color formatted Berlin Clock rows
+// Check GitHub commit
 
 public class BerlinClockFace extends BerlinClock {
     private final String LAMP_YELLOW = "Y";
